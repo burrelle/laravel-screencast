18c5afb9d67c81550b2f61aa9d43d4fc9a201f71
burrelle <burrelle@oregonstate.edu>
2018-01-19 20:56:58 -0800

    Added comment form and validation

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,9 +1,33 @@
-@extends('layout')
-
+@extends('layout') 
 @section('content')
-  <div class="col-sm-8 blog-main">
-    <h1>{{ $post->title }}</h1>
-
-    {{ $post->body }}
+<div class="col-sm-8 blog-main">
+  <h1>{{ $post->title }}</h1>
+  {{ $post->body }}
+  <hr>
+  <div class="comment">
+    <ul class="list-group">
+      @foreach ($post->comments as $comment)
+      <li class="list-group-item">
+        <strong>{{ $comment->created_at->diffForHumans() }}</strong> {{ $comment->body }}
+      </li>
+      @endforeach
+    </ul>
+    <hr> 
+    {{-- Add a comment --}}
+    <div class="card">
+      <div class="card-block container-fluid">
+        <form action="/posts/{{ $post->id }}/comments" method="POST">
+          {{ csrf_field() }}
+          <div class="form-group">
+              <textarea name="body" id="body" class="form-control" placeholder="Your comment here..."></textarea>
+          </div>
+          <div class="form-group">
+            <button class="btn btn-primary" type="submit">Add comment</button>
+          </div>
+          @include('layouts.errors')
+        </form>
+      </div>
+    </div>
   </div>
+</div>
 @endsection
